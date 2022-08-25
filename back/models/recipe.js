dd863c79aede1a48bf66a2aabc7e91cb00fd6cb4
Lily <justpunkrock@hotmail.fr>
2022-08-25 11:43:38 +0200

    Model update

--- back/models/recipe.js
+++ back/models/recipe.js
@@ -1,19 +1,21 @@
+//Importation du package Mongoose
 const mongoose = require('mongoose');
 
+//Création du schéma
 const recipeSchema = mongoose.Schema({
   //  _id: string,  automatique via mongodb
-  name: String,
-  manufacturer: String,
-  description: String,
-  heat: Number,
-  likes: Number,
-  dislikes: Number,
-  mainPepper: String,
-  usersLiked: Array,
-  usersDisliked: Array,
-  userId: String,
-  imageUrl: String
+  userId :{type: String, required: true},
+    name: { type: String, required: true},
+    manufacturer: { type: String, required: true},
+    description: { type: String, required: true},
+    mainPepper: { type: String, required: true},
+    imageUrl: { type: String, required: true},
+    heat: { type: Number, required: true},
+    likes: { type: Number, required: true, default: 0},
+    dislikes: { type: Number, required: true, default: 0},
+    usersLiked : {type : [String], required : true, default: 0},
+    usersDisliked : {type : [String], required : true, default: 0},
 });
 
-
+//Exportation du schéma recipe
 module.exports = mongoose.model('Recipe', recipeSchema);
