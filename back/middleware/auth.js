a4df4e9dd6de50fce7306e1597e0a1da20eb3b09
Lily <justpunkrock@hotmail.fr>
2022-08-25 15:21:26 +0200

    Look for mistake

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -1,10 +1,15 @@
+//Importation du package JsonWebToken
 const jwt = require('jsonwebtoken');
 
+//Exportation des middlewares
 module.exports = (req, res, next) => {
     try {
+        //Récupération du token après bearer
         const token = req.headers.authorization.split(' ')[1];
+        //Décode le token et vérifie
         const decodedToken = jwt.verify(token, 'P6-Secret-Key');
         const userId = decodedToken.userId;
+        //compare les userId
         if (req.body.userId && req.body.userId !== userId) {
             throw 'User Id not usable';
         } else {
--- back/middleware/multer-config.js
+++ back/middleware/multer-config.js
@@ -1,20 +1,26 @@
+//Importation du package multer
 const multer = require('multer');
+const path = require('path');
 
+//Types de fichiers acceptés
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png',
 };
+const chemin = path.join(__dirname , '../images');
 
+//Création de l'objet de configuration de multer
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        callback(null,'images');
+        callback(null, chemin);
     },
-    filename: (req,file,callback)=> {
+    filename: (req, file, callback) => {
         const name = file.originalname.split(' ').join('_');
         const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '-' + extension);
+        callback(null, name + Date.now() + "." + extension);
     }
 });
 
-module.exports = multer({storage:storage}).single('image');
+//exportation du Middleware
+module.exports = multer({ storage: storage }).single('image');
