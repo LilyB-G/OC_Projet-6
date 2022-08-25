380e1bed320da96ae92df55490cd7a10f6e5c31c
Lily <justpunkrock@hotmail.fr>
2022-08-25 15:23:44 +0200

    user.js OK

--- back/routes/recipe.js
+++ back/routes/recipe.js
@@ -1,12 +1,16 @@
+//Importation du router d'Express
 const express = require('express');
 const router = express.Router();
+
+//Importation des Middleware 'auth' et 'multer'
 const auth = require('../middleware/auth')
 const multer = require('../middleware/multer-config');
 
+//Importation des controllers
 const recipeCtrl = require('../controllers/recipe');
 
 
-//Select All
+//Select All définition des Routers
 router.get('/', auth, recipeCtrl.getAllRecipe);
 //Create
 router.post('/', auth , multer , recipeCtrl.createRecipe);
@@ -17,9 +21,7 @@ router.put('/:id', auth , multer, recipeCtrl.updateRecipe);
 //Delete
 router.delete('/:id', auth , recipeCtrl.deleteRecipe);
 
+router.post('/:id/like' , auth, recipeCtrl.functionlike);
 
-
-
-
-
+//Exportation des Routers
 module.exports = router;
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,15 +1,14 @@
-
+//assignation des Controllers
 const userCtrl = require('../controllers/user');
+//Importation du package router d'Express
 const express = require('express');
 const router = express.Router();
 
-
-//new user Signup
+//new user Signup définition du chemin
 router.post('/signup',  userCtrl.signup );
 
-
-//Existant user login
+//Existant user login définition du chemin
 router.post('/login',  userCtrl.login );
 
-
+//Exportation du router
 module.exports = router;
