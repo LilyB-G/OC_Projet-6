6eb9eec745b06d88ff9824f77f82595f09e75d39
Lily <justpunkrock@hotmail.fr>
2022-08-31 16:58:53 +0200

    Updates on files

--- back/app.js
+++ back/app.js
@@ -1,26 +1,32 @@
 const express = require('express'); //Importation du apckage Express
 const app = express(); //Appel de la méthode Express
-const cors = require('cors');
+//const cors = require('cors');
 const path = require('path'); //Importation de 'path' pour définir les chemins
 const mongoose = require('mongoose'); //Importation du package Mongoose (sert à définir les types de variables et à structurer les données)
 const recipeRoute = require('./routes/recipe'); //importation du router "recipe"
 const userRoute = require('./routes/user'); //importation du router "user"
-const morgan = require('morgan') //middleware qui enregistre les requests
+const morgan = require('morgan'); //middleware qui enregistre les requests
+require('dotenv').config()
 
 
-morgan.token('body',(req => JSON.stringify (req.body)));
+morgan.token('body', (req => JSON.stringify(req.body)));
 
 app.use(morgan(':url :method :body '));
 
+app.use(express.urlencoded({ extended: true }));//add this before any route or before using req.body
+
 app.use(express.json());
 
-  //Connexion à la base de données avec password et id
-mongoose.connect('mongodb+srv://teddybear:dGG9W47VcfjDEPv@project6.sbm3pe6.mongodb.net/?retryWrites=true&w=majority',
+//Connexion à la base de données avec password et id
+const password = process.env.DB_PASSWORD;
+const login = process.env.DB_USERNAME;
+
+mongoose.connect('mongodb+srv://' + login + ':' + password + '@project6.sbm3pe6.mongodb.net/?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    
+
     .then(() => console.log('Connection MongoDB ok'))
     .catch(() => console.log('MongoDB connection failed'));
 
@@ -32,15 +38,16 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/', cors());
-app.options('*', cors());
+//app.use('/', cors());
+//app.options('*', cors());
 
 // Gestion des routes
 app.use('/api/sauces', recipeRoute);
-app.use('/api/auth', userRoute ); 
+app.use('/api/auth', userRoute);
 
 //dossier images
 app.use('/images', express.static(path.join(__dirname, '/images')));
+
 
 //exportation de la constante app
 module.exports = app;
--- back/middleware/multer-config.js
+++ back/middleware/multer-config.js
@@ -16,7 +16,9 @@ const storage = multer.diskStorage({
         callback(null, chemin);
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
+        //const name = file.originalname.split(' ').join('_');
+        let str = file.originalname.split(' ').join('_');
+        const name = str.substring(0,str.indexOf('.'));
         const extension = MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + "." + extension);
     }
